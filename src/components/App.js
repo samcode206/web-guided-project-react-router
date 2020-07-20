2b971987dee971ae950b0861eb220637a5c9751c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-20 18:49:33 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -40,7 +40,7 @@ export default function App(props) {
       </nav>
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
-      <Route path='/'>
+      <Route exact path='/'>
         <Home />
       </Route>
 
